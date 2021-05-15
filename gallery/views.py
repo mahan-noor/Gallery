ee328cec5c2c709a536fc1c73c07b1aa770b0bed
mahan-noor <ismahanabey@gmail.com>
2021-05-15 22:06:20 +0300

    add template files and function gallery in view

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def gallery(request):
+    images = Image.get_all_images()
+    location = Location.object.all()
+    return render(request, )
