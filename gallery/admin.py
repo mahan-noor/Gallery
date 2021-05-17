ebe9a6a809c8a55a124eaf5e2892a6ad139c03fa
mahan-noor <ismahanabey@gmail.com>
2021-05-17 08:49:45 +0300

    add admin.site.register function

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Editor,Location,Image,Category
 # Register your models here.
+
+admin.site.register(Editor)
+admin.site.register(Location)
+admin.site.register(Image)
+admin.site.register(Category)
