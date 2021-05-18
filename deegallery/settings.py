0e953a1199a7428ae898f9007b40c398b1ea9a01
mahan-noor <ismahanabey@gmail.com>
2021-05-18 17:36:08 +0300

    deployment

--- deegallery/settings.py
+++ deegallery/settings.py
@@ -24,7 +24,7 @@ DEBUG = config('DEBUG')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Quick-start development settings - unsuitable for production
@@ -114,7 +114,6 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Password validation
