75a36a99af589036120982695d7079467d4cff54
mahan-noor <ismahanabey@gmail.com>
2021-05-18 17:29:00 +0300

    deployment

--- deegallery/settings.py
+++ deegallery/settings.py
@@ -162,8 +162,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
